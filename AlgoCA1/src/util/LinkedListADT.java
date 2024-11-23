b2825f6227936cd20b5b85677d624529cb61a8ca
AndrewRickerby <156925854+AndrewRickerby@users.noreply.github.com>
2024-11-23 21:25:14 +0000

    Update LinkedListADT.java

--- AlgoCA1/src/util/LinkedListADT.java
+++ AlgoCA1/src/util/LinkedListADT.java
@@ -1,8 +1,16 @@
+/*
+* AlgoCA1 :
+* Andrew Rickerby :
+* C23344333 :
+* Description of class :
+*/
+
+
 package util;
 
 public interface LinkedListADT<T> {
     void add(T element);
-    void add(T element, int position); 
+    void add(T element, int position); // Overloaded add for specific positions
     T remove();
     boolean isEmpty();
     int size();
