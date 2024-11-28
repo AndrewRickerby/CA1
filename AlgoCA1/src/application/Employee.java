62e89fee5df4d6bf6485f33b89e3e7b9619aa128
AndrewRickerby <156925854+AndrewRickerby@users.noreply.github.com>
2024-11-28 13:42:04 +0000

    Update Employee.java

--- AlgoCA1/src/application/Employee.java
+++ AlgoCA1/src/application/Employee.java
@@ -2,7 +2,7 @@
 * AlgoCA1 :
 * Andrew Rickerby :
 * C23344333 :
-* Description of class :
+* Description of class :class represents attributes for name, code, course, and years of experience, along with methods to access and modify these details.
 */
 
 
@@ -12,17 +12,20 @@ public class Employee {
     private String name;
     private String code;
     private String course;
+    private int years;
 
     public Employee() {
         this.name = "";
         this.code = "";
         this.course = "";
+        this.years = 0;
     }
 
-    public Employee(String name, String code, String course) {
+    public Employee(String name, String code, String course, int years) {
         this.name = name;
         this.code = code;
         this.course = course;
+        this.years = years;
     }
 
     public String getName() {
@@ -35,6 +38,10 @@ public class Employee {
 
     public String getCourse() {
         return course;
+    }
+    
+    public int getYears() {
+        return years;
     }
 
     public void setName(String name) {
@@ -49,10 +56,14 @@ public class Employee {
         this.course = course;
     }
 
+    public void setYears(int years) {
+        this.years = years;
+    }
     @Override
     public String toString() {
         return "name: " + name + "\n" +
                "emplyee code: " + code + "\n" +
-               "course: " + course;
+               "course: " + course + "\n" +
+        	   "years of experience:" + years;
     }
 }
